dad57f7f92fc9b36488c2efa0da2b05705a7d35c
Vgony <118470864+Of-Arte@users.noreply.github.com>
2026-02-23 08:04:38 -0500

    chore: Update React to 19.2.1 to resolve CVE-2025-55182

--- components/icons/ObsidianLogo.tsx
+++ components/icons/ObsidianLogo.tsx
@@ -3,12 +3,18 @@ import React from 'react';
 const ObsidianLogo: React.FC<{ className?: string }> = ({ className = "w-6 h-6" }) => {
     return (
         <svg
-            viewBox="0 0 24 24"
+            viewBox="0 0 256 256"
             className={className}
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <rect width="24" height="24" fill="black" />
+            {/* Top facets */}
+            <path d="M128 16 L208 80 L128 144 Z" fill="#a78bfa" />
+            <path d="M128 16 L48 80 L128 144 Z" fill="#8b5cf6" />
+
+            {/* Bottom facets */}
+            <path d="M48 80 L128 240 L128 144 Z" fill="#7c3aed" />
+            <path d="M208 80 L128 240 L128 144 Z" fill="#5b21b6" />
         </svg>
     );
 };
